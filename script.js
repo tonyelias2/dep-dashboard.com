437e25ef9042b21aa3128280f99b42856559387b
tonyelias2 <91190444+tonyelias2@users.noreply.github.com>
2021-09-28 18:45:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -29,3 +29,14 @@ function calculateBMI(){
          }
     }
 }
+
+function calculateLoan(){
+    const amount = document.querySelector('#amount').value;
+    const interest_rate = document.querySelector('#interest').value;
+    const months = document.querySelector('#months').value;
+    const interest = (amount * (interest_rate * 0.01)) / months;
+    const payment = ((amount / months) + interest).toFixed(2);
+
+
+    document.querySelector('#payment').innerHTML = '$' + payment + '/ Month';
+}
